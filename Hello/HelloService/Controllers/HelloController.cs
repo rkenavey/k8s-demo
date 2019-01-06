f71a8bbee8e3580472c3e057ec5cdc0f1ae8fda4
Ryan Kenavey <ryan@olo.com>
2019-01-05 22:52:42 -0500

    Service: Implement hello

---
+++ Hello/HelloService/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloService.Controllers
+{
+    [Route("[controller]")]
+    public class HelloController : Controller
+    {
+        // GET hello/world
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return $"Hello {name}";
+        }
+    }
+}
