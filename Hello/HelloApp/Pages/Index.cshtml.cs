ee8a50977313004806eb9ad59d57d88c5cf3396f
Ryan Kenavey <ryan@olo.com>
2019-01-06 00:22:51 -0500

    App: Call service for message

--- Hello/HelloApp/Pages/Index.cshtml.cs
+++ Hello/HelloApp/Pages/Index.cshtml.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,6 +10,10 @@ namespace HelloApp.Pages
 {
     public class IndexModel : PageModel
     {
+        // TODO: Service discovery
+        private static readonly Uri HelloServiceEndpoint = new Uri(@"https://localhost:5001/hello/");
+        private static readonly HttpClient HttpClient = new HttpClient();
+
         [BindProperty]
         [Required, StringLength(100)]
         public string Name { get; set; }
@@ -15,14 +23,18 @@ namespace HelloApp.Pages
 
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
 
-            ViewData["Message"] = $"Hello {Name}";
+            var uri = new Uri(HelloServiceEndpoint, Name);
+            Console.WriteLine($"Calling {uri}");
+            var message = await HttpClient.GetStringAsync(uri);
+
+            ViewData["Message"] = message;
             return Page();
         }
     }
